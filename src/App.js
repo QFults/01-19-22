d364d058ec09ff10d1a508090a7c7c088c70c845
Quinton Fults <quinton.fults.work@gmail.com>
2022-01-19 11:59:59 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,70 +1,27 @@
-import { useState } from 'react'
-import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
 
 const App = () => {
-  const [movieState, setMovieState] = useState({
-    title: '',
-    movie: {},
-    movies: []
-  })
-
-  const handleInputChange = ({ target: { name, value } }) => setMovieState({ ...movieState, [name]: value })
-
-  const handleSearchMovie = event => {
-    event.preventDefault()
-    axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${movieState.title}`)
-      .then(({ data: movie }) => {
-        console.log(movie)
-        setMovieState({ ...movieState, movie, title: '' })
-      })
-      .catch(err => console.error(err))
-  }
-
-  const handleSaveMovie = () => {
-    const movies = JSON.parse(JSON.stringify(movieState.movies))
-    movies.push(movieState.movie)
-    setMovieState({ ...movieState, movies, movie: {} })
-  }
   return (
-    <div class="container">
-      <div className="row">
-        <form>
-          <label htmlFor="title">title</label>
-          <input
-            type="text"
-            name="title"
-            value={movieState.title}
-            onChange={handleInputChange} />
-          <button onClick={handleSearchMovie}>Search</button>
-        </form>
+    <Router>
+      <div>
+        <Link to='/'>About</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/portfolio'>Portfolio</Link>
       </div>
-      <hr />
-      <div class="row">
-        <div className="col-md-6">
-          {
-            movieState.movie.Title ? (
-              <div>
-                <h1>{movieState.movie.Title} <button onClick={handleSaveMovie}>Save Movie</button></h1>
-                <h3>Released On {movieState.movie.Released}</h3>
-                <p>{movieState.movie.Plot}</p>
-              </div>
-            ) : null
-          }
-        </div>
-        <div className="col-md-6">
-          <h4>Saved Movies</h4>
-          {
-            movieState.movies.map(movie => (
-              <div>
-                <h1>{movie.Title}</h1>
-                <h3>Released On {movie.Released}</h3>
-                <p>{movie.Plot}</p>
-              </div>
-            ))
-          }
-        </div>
-      </div>
-    </div>
+      <Routes>
+        <Route path='/' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+      </Routes>
+    </Router>
   )
 }
 
